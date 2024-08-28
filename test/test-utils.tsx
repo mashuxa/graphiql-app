e6f4b971cbca59f7142f5051c42cc4f483c57f94
Dmitrii Barilkin <dimabaril@gmail.com>
2024-08-28 15:28:25 +0200

    refactor: test-utils refactor

--- test/test-utils.tsx
+++ test/test-utils.tsx
@@ -1,65 +1,11 @@
 import { render, RenderResult } from "@testing-library/react";
 import { NextIntlClientProvider } from "next-intl";
 import { ReactNode } from "react";
-
-const messages = {
-  LocaleSwitcher: {
-    en: "English",
-    ru: "Russian",
-  },
-  AuthButton: {
-    signOut: "Sign Out",
-    signIn: "Sign In",
-  },
-  Greeting: {
-    "auth-greeting": "Welcome Back, {username}!",
-    "unauth-greeting": "Welcome!",
-    signIn: "Sign In",
-    signUp: "Sign Up",
-    restClient: "REST client",
-    graphiqlClient: "GraphiQL client",
-    history: "History",
-  },
-  GraphiQL: {
-    placeholder: "GraphiQL client",
-  },
-  History: {
-    placeholder: "History",
-  },
-  RestClient: {
-    placeholder: "Rest client",
-  },
-  ResetPassword: {
-    title: "Reset password",
-    email: "Email",
-    submit: "Reset",
-    signIn: "Sign In",
-  },
-  SignIn: {
-    title: "Sign In",
-    email: "Email",
-    password: "Password",
-    submit: "Sign In",
-    resetPassword: "Reset password",
-    signUp: "Sign Up",
-  },
-  SignUp: {
-    title: "Sign Up",
-    name: "Name",
-    email: "Email",
-    password: "Password",
-    repeatPassword: "Repeat password",
-    submit: "Sign Up",
-    signIn: "Sign In",
-  },
-  publicLayout: {
-    signInWithGoogle: "Sign In with Google",
-  },
-};
+import messageEn from "../src/messages/en.json";
 
 const AllProviders = ({ children }: { children: ReactNode }): JSX.Element => {
   return (
-    <NextIntlClientProvider locale="en" messages={messages}>
+    <NextIntlClientProvider locale="en" messages={messageEn}>
       {children}
     </NextIntlClientProvider>
   );
