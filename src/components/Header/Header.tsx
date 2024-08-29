5a95513ab443c65fee73a8a2d183efc6c520ab7f
Dmitrii Barilkin <dimabaril@gmail.com>
2024-08-29 21:55:56 +0200

    feat: change header style when header is pinned

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,8 @@
+"use client";
+
 import { useLocale } from "next-intl";
 import Image from "next/image";
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import AuthButton from "src/components/Header/AuthButton/AuthButton";
 import LanguageToggle from "src/components/LanguageToggle/LanguageToggle";
 import { routes } from "src/constants";
@@ -8,10 +10,30 @@ import { Link, Locale } from "src/i18n.config";
 
 const Header: FC = () => {
   const locale = useLocale() as Locale;
+  const [isPinned, setIsPinned] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = (): void => {
+      if (window.scrollY > 0) {
+        setIsPinned(true);
+      } else {
+        setIsPinned(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return (): void => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   return (
-    <header data-testid="header" className="sticky top-0 bg-neutral-800">
-      <ul className="flex items-center px-2 py-4 text-white">
+    <header
+      data-testid="header"
+      className={`sticky top-0 px-2 ${isPinned ? "py-1" : "py-4"} bg-neutral-800 transition-all duration-300`}
+    >
+      <ul className="flex items-center text-white">
         <li className="mr-auto">
           <Link href={routes.home}>
             <Image src="/rss-logo.svg" width={48} height={48} alt="RSS Logo" />
