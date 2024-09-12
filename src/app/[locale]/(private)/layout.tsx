a79f463512e4d147ae821c5217c396500159d23a
mashuxa <9364688@gmail.com>
2024-09-12 21:49:10 +0200

    feat: move redirect logic to AuthRedirect component

--- src/app/[locale]/(private)/layout.tsx
+++ src/app/[locale]/(private)/layout.tsx
@@ -1,13 +1,15 @@
 "use client";
 
 import { FC, PropsWithChildren } from "react";
-import { routes } from "src/constants";
-import useAuthRedirect from "src/hooks/useAuthRedirect/useAuthRedirect";
+import AuthRedirect from "src/components/AuthRedirect/AuthRedirect";
 
 const Layout: FC<PropsWithChildren> = ({ children }) => {
-  useAuthRedirect(false, routes.signIn);
-
-  return children;
+  return (
+    <>
+      <AuthRedirect />
+      {children}
+    </>
+  );
 };
 
 export default Layout;
