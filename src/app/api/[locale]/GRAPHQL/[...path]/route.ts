e106ad64e7d6bfbcdcff44bc4bb015fcf9e23269
mashuxa <9364688@gmail.com>
2024-09-13 15:26:17 +0200

    feat: move route handler to common handler

--- src/app/api/[locale]/GRAPHQL/[...path]/route.ts
+++ src/app/api/[locale]/GRAPHQL/[...path]/route.ts
@@ -1,6 +1,6 @@
-import { NextRequest, NextResponse } from "next/server";
-import { FetchOptions, HttpMethod } from "src/types";
-import { decodeFromBase64 } from "src/utils/utils";
+import { NextRequest } from "next/server";
+import { handleFetch } from "src/app/api/[locale]/handleFetch";
+import { HttpMethod } from "src/types";
 
 interface Params {
   params: { path: [string, string] };
@@ -9,34 +9,4 @@ interface Params {
 export const GET = async (
   request: NextRequest,
   { params: { path } }: Params,
-): Promise<Response> => {
-  const [urlBase64, bodyBase64] = path;
-  const searchParams = request.nextUrl.searchParams.entries();
-
-  if (!urlBase64) {
-    throw Error("URL is required");
-  }
-
-  const url = decodeFromBase64(urlBase64);
-  const body = decodeFromBase64(bodyBase64);
-  const headers: Record<string, string> = Object.fromEntries(searchParams);
-
-  headers["Content-Type"] = "application/json";
-
-  const options: FetchOptions = {
-    method: HttpMethod.post,
-    headers,
-    body: JSON.stringify({ query: body }),
-  };
-
-  try {
-    const response = await fetch(url, options);
-    const data = response.ok ? await response.json() : await response.text();
-
-    return NextResponse.json(data, { status: response.status });
-  } catch (error) {
-    // обработать ошибки по таймауту, сервер не доступен и тп
-    console.error("Error fetching data:", error);
-    throw error;
-  }
-};
+): Promise<Response> => handleFetch(request, [HttpMethod.post, ...path], true);
---
+++ src/app/api/[locale]/handleFetch.ts
@@ -0,0 +1,57 @@
+import { NextRequest, NextResponse } from "next/server";
+import { FetchOptions, HttpMethod } from "src/types";
+import { decodeFromBase64 } from "src/utils/utils";
+
+const methodsWithBody = [
+  HttpMethod.post,
+  HttpMethod.put,
+  HttpMethod.patch,
+  HttpMethod.delete,
+];
+
+type Params = [HttpMethod, string, string];
+
+export const handleFetch = async (
+  request: NextRequest,
+  params: Params,
+  isGraphql: boolean,
+): Promise<Response> => {
+  const [method, urlBase64, bodyBase64] = params;
+  const searchParams = request.nextUrl.searchParams.entries();
+
+  if (!method) {
+    throw Error("Method is required");
+  }
+
+  if (!urlBase64) {
+    throw Error("URL is required");
+  }
+
+  const url = decodeFromBase64(urlBase64);
+  const body = decodeFromBase64(bodyBase64);
+  const headers: Record<string, string> = Object.fromEntries(searchParams);
+
+  // @todo: json || text
+  if (methodsWithBody.includes(method) && body) {
+    headers["Content-Type"] = "application/json";
+  }
+
+  const options: FetchOptions = { method, headers };
+
+  if (isGraphql) {
+    options.body = JSON.stringify({ query: body });
+  } else if (methodsWithBody.includes(method) && body) {
+    options.body = body;
+  }
+
+  try {
+    const response = await fetch(url, options);
+    const data = response.ok ? await response.json() : await response.text();
+
+    return NextResponse.json(data, { status: response.status });
+  } catch (error) {
+    // обработать ошибки по таймауту, сервер не доступен и тп
+    console.error("Error fetching data:", error);
+    throw error;
+  }
+};
