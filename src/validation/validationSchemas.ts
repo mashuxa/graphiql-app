5b02bdedb295b2bcfab6c0ec759200b8eee959ea
Dmitrii Barilkin <dimabaril@gmail.com>
2024-09-15 01:52:05 +0200

    just save

--- src/validation/validationSchemas.ts
+++ src/validation/validationSchemas.ts
@@ -1,14 +1,20 @@
 import { httpMethodList } from "src/types";
 import * as yup from "yup";
 
-const headerKeyRegex = /^[a-zA-Z0-9-]+$/;
-const headerValueRegex = /^[^\x00-\x1F\x7F]*$/;
+export const headerKeyRegex = /^[a-zA-Z0-9-]+$/;
+export const headerValueRegex = /^[^\x00-\x1F\x7F]*$/;
 
 export const loginSchema = yup.object().shape({
   email: yup.string().email("Invalid email").required("Email is required"),
   password: yup
     .string()
-    .min(6, "Password must be at least 6 characters")
+    .min(8, "Password must be at least 8 characters long")
+    .matches(/\p{L}/u, "Password must contain at least one letter")
+    .matches(/\d/, "Password must contain at least one digit")
+    .matches(
+      /\p{P}|\p{S}/u,
+      "Password must contain at least one special character",
+    )
     .required("Password is required"),
 });
 
@@ -17,7 +23,13 @@ export const registrationSchema = yup.object().shape({
   email: yup.string().email("Invalid email").required("Email is required"),
   password: yup
     .string()
-    .min(6, "Password must be at least 6 characters")
+    .min(8, "Password must be at least 8 characters long")
+    .matches(/\p{L}/u, "Password must contain at least one letter")
+    .matches(/\d/, "Password must contain at least one digit")
+    .matches(
+      /\p{P}|\p{S}/u,
+      "Password must contain at least one special character",
+    )
     .required("Password is required"),
   repeatPassword: yup
     .string()
@@ -31,32 +43,32 @@ export const restSchema = yup.object().shape({
     .oneOf(httpMethodList, "Should be Http method")
     .required("Method is required"),
   url: yup.string().url("Invalid URL").required("URL is required"),
-  headers: yup.array().of(
-    yup.object().shape({
-      key: yup
-        .string()
-        .matches(
-          headerKeyRegex,
-          "Header key must contain only Latin letters, digits, and hyphens",
-        )
-        .required("Header key is required"),
-      value: yup
-        .string()
-        .matches(headerValueRegex, "Header value contains invalid characters")
-        .required("Header value is required"),
-    }),
-  ),
+  //   headers: yup.array().of(
+  //     yup.object().shape({
+  //       key: yup
+  //         .string()
+  //         .matches(
+  //           headerKeyRegex,
+  //           "Header key must contain only Latin letters, digits, and hyphens",
+  //         )
+  //         .required("Header key is required"),
+  //       value: yup
+  //         .string()
+  //         .matches(headerValueRegex, "Header value contains invalid characters")
+  //         .required("Header value is required"),
+  //     }),
+  //   ),
   body: yup.string(),
-  variables: yup.array().of(
-    yup.object().shape({
-      key: yup
-        .string()
-        .matches(
-          headerKeyRegex,
-          "Header key must contain only Latin letters, digits, and hyphens",
-        )
-        .required("Variable key is required"),
-      value: yup.string().required("Variable value is required"),
-    }),
-  ),
+  //   variables: yup.array().of(
+  //     yup.object().shape({
+  //       key: yup
+  //         .string()
+  //         .matches(
+  //           headerKeyRegex,
+  //           "Header key must contain only Latin letters, digits, and hyphens",
+  //         )
+  //         .required("Variable key is required"),
+  //       value: yup.string().required("Variable value is required"),
+  //     }),
+  //   ),
 });
